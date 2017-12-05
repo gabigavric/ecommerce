bb66be04d9cce4da748eb937472a8b205196f992
gabrijela gavric <gabigavric@gmail.com>
2017-12-05 09:49:34 -0800

    add initial relationships and landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root :to => 'landing_page#welcome'
   resources :accounts
   resources :order_items
   resources :orders
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
