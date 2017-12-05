f05a8c94e9f2c3fada8aaf382dd1c726f9a1b4c0
gabrijela gavric <gabigavric@gmail.com>
2017-12-05 09:16:14 -0800

    add initial files with a seed for products only

---
+++ spec/views/orders/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "orders/new", type: :view do
+  before(:each) do
+    assign(:order, Order.new(
+      :status => "MyString",
+      :account_id => 1,
+      :total_price => "9.99"
+    ))
+  end
+
+  it "renders new order form" do
+    render
+
+    assert_select "form[action=?][method=?]", orders_path, "post" do
+
+      assert_select "input[name=?]", "order[status]"
+
+      assert_select "input[name=?]", "order[account_id]"
+
+      assert_select "input[name=?]", "order[total_price]"
+    end
+  end
+end
